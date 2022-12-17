637e5c2990c4b415e5e4d0099336f2b6201c1580
jcribeiro <j.alberto.cribeiro@gmail.com>
2022-12-17 17:25:38 +0000

    Created attributes and initial methods for the Airport class

--- src/Airport.h
+++ src/Airport.h
@@ -5,9 +5,68 @@
 #ifndef PROJ2_AIRPORT_H
 #define PROJ2_AIRPORT_H
 
+#include <string>
+using namespace std;
 
+/**
+ * Represents an Airport
+ */
 class Airport {
 
+    string code;
+    string name;
+    string city;
+    string country;
+    float latitude;
+    float longitude;
+
+public:
+    /**
+     * Creates a new Airport object
+     * @param code The unique IATA code for the airport
+     * @param name The official name of the airport
+     * @param city The city where the airport is located
+     * @param country The country where the airport is located
+     * @param latitude The latitude coordinate for the airport
+     * @param longitude The longitude coordinate for the airport
+     */
+    Airport(string code, string name, string city, string country, float latitude, float longitude);
+
+    /**
+     * Returns the IATA code for the airport
+     * @return the IATA code for the airport
+     */
+    string getCode() const;
+
+    /**
+     * Returns the official name of the airport
+     * @return the official name of the airport
+     */
+    string getName() const;
+
+    /**
+     * Returns the city where the airport is located
+     * @return the city where the airport is located
+     */
+    string getCity() const;
+
+    /**
+     * Returns the country where the airport is located
+     * @return the country where the airport is located
+     */
+    string getCountry() const;
+
+    /**
+     * Returns the latitude coordinate for the airport
+     * @return the latitude coordinate for the airport
+     */
+    float getLatitude() const;
+
+    /**
+     * Returns the longitude coordinate for the airport
+     * @return the longitude coordinate for the airport
+     */
+    float getLongitude() const;
 };
 
 
